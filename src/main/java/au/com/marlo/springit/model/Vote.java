6922b704a8b63a393b285759ce9a8a57803883f2
EduardoZimelewicz <eduardo.zimelewicz@gmail.com>
2019-06-28 11:03:42 -0300

    Removing NonNull annotation from a primitive

--- src/main/java/au/com/marlo/springit/model/Vote.java
+++ src/main/java/au/com/marlo/springit/model/Vote.java
@@ -2,7 +2,6 @@ package au.com.marlo.springit.model;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.NonNull;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -16,6 +15,6 @@ public class Vote {
     @Id
     @GeneratedValue
     private long id;
-    @NonNull
+
     private int vote;
 }
