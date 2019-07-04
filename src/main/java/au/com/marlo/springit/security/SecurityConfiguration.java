03218c2d55b993ccb6013f1530920cdc0333c01b
EduardoZimelewicz <eduardo.zimelewicz@gmail.com>
2019-07-04 18:28:41 -0300

    login functional

--- src/main/java/au/com/marlo/springit/security/SecurityConfiguration.java
+++ src/main/java/au/com/marlo/springit/security/SecurityConfiguration.java
@@ -22,18 +22,23 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .requestMatchers(EndpointRequest.to("info")).permitAll()
-                .requestMatchers(EndpointRequest.toAnyEndpoint()).hasRole("ADMIN")
-                .antMatchers("/actuator/").hasRole("ADMIN")
-                .antMatchers("/").permitAll()
-                .antMatchers("/link/submit").hasRole("USER")
-                .antMatchers("/link/**").permitAll()
-                .antMatchers("/h2-console/**").permitAll()
+                    .requestMatchers(EndpointRequest.to("info")).permitAll()
+                    .requestMatchers(EndpointRequest.toAnyEndpoint()).hasRole("ADMIN")
+                    .antMatchers("/actuator/").hasRole("ADMIN")
+                    .antMatchers("/").permitAll()
+                    .antMatchers("/link/submit").hasRole("USER")
+                    .antMatchers("/link/**").permitAll()
+                    .antMatchers("/h2-console/**").permitAll()
                 .and()
-                .formLogin()
+                    .formLogin().loginPage("/login").permitAll()
+                    .usernameParameter("email")
                 .and()
-                .csrf().disable()
-                .headers().frameOptions().disable();
+                .logout()
+                .and()
+                .rememberMe();
+                //.and()
+                //    .csrf().disable()
+                //    .headers().frameOptions().disable();
     }
 
 
